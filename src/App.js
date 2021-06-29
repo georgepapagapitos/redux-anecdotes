a110c5008cf745bab23aeaba4bafd3416c67a06d
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-29 12:17:39 -0500

    mock db with json-server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
+import { getAll } from './services/anecdotes';
+import { initializeAnecdotes } from './reducers/anecdote.reducer';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getAll().then(
+      anecdotes => dispatch(initializeAnecdotes(anecdotes))
+    )
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdote.reducer';
 import { clear, error, success } from '../reducers/notification.reducer';
+import { createNew } from '../services/anecdotes';
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
     try {
-      dispatch(createAnecdote(content));
+      const newAnecdote = await createNew(content);
+      dispatch(createAnecdote(newAnecdote));
       dispatch(success(`added a new anecdote!`));
     } catch (e) {
       dispatch(error('error adding a new anecdote'))
